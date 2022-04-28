2a6f059ebce2009d0c7994922b189e80e0d6ac66
Rishav Nath Pati <patirishavnath@gmail.com>
2022-04-28 15:48:35 +0530

    Pokemon Puzzle

---
+++ Games/Pokemon-Puzzle/cfg.py
@@ -0,0 +1,17 @@
+
+import os
+
+SCREENSIZE = (640, 640)
+
+PICTURE_ROOT_DIR = os.path.join(os.getcwd(), 'resources/pictures')
+
+FONTPATH = os.path.join(os.getcwd(), 'resources/font/FZSTK.TTF')
+
+BACKGROUNDCOLOR = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+
+FPS = 40
+
+RANDNUM = 100
